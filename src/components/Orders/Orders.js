dd187615618f671ca775036349690fa2ee6b7b40
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-04-01 00:50:57 +0600

    create hooks

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
+    const [products, setProducts] = useProducts()
     return (
         <div>
             <h1>Orders</h1>
+            <h2>total products: {products.length}</h2>
         </div>
     );
 };
